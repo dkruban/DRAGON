832b9da7ca8ec5bf0a57bf9e70296bc65a84546b
Ruban.M <dkrkumar8585@gmail.com>
2025-09-15 19:45:57 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-ECHO is off.
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import App from './components/App';
+import { AuthProvider } from './contexts/AuthContext';
+import { ThemeProvider as CustomThemeProvider } from './contexts/ThemeContext';
+import darkTheme from './components/UI/DarkTheme';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <CustomThemeProvider>
+          <AuthProvider>
+            <App />
+          </AuthProvider>
+        </CustomThemeProvider>
+      </ThemeProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
