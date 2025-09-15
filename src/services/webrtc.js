775f85344a8134fdf2e8883912a64e84d70f56e4
Ruban.M <dkrkumar8585@gmail.com>
2025-09-15 19:52:10 +0530

    Update webrtc.js

--- src/services/webrtc.js
+++ src/services/webrtc.js
@@ -1,1 +1,31 @@
-ECHO is off.
+import Peer from 'simple-peer';
+
+export function createPeer(initiator, stream) {
+  return new Peer({
+    initiator,
+    stream,
+    trickle: false,
+    config: {
+      iceServers: [
+        { urls: 'stun:stun.l.google.com:19302' },
+        { urls: 'stun:global.stun.twilio.com:3478' }
+      ]
+    }
+  });
+}
+
+export function setupCall(peer, remoteVideoRef) {
+  peer.on('stream', (stream) => {
+    if (remoteVideoRef.current) {
+      remoteVideoRef.current.srcObject = stream;
+    }
+  });
+
+  peer.on('close', () => {
+    if (remoteVideoRef.current) {
+      remoteVideoRef.current.srcObject = null;
+    }
+  });
+
+  return peer;
+}
